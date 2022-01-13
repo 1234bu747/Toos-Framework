8fc3f0c0bda22c6d1e98832685dde52111d0131a
王萧牧 <1092056182@qq.com>
2022-01-13 22:47:54 +0800

    change 进度条.py

--- 进度条.py
+++ 进度条.py
@@ -1,7 +1,7 @@
 # -*-coding:utf-8 -*-
 import time
 
-scale = ["*"]*50  # 总量，和第一个类似
+scale = ["*"] * 50  # 总量，和第一个类似
 # print("执行开始".center(len(scale)//2,"-"))#产生这个-----------执行开始----------
 t = time.perf_counter()
 for i in range(len(scale) + 1):  # 50不能打印出来，要加+1
@@ -10,5 +10,5 @@ for i in range(len(scale) + 1):  # 50不能打印出来，要加+1
     c = (i / len(scale)) * 100  # 百分比进度 50%那种
     t = time.perf_counter()  # 消耗时间
     print("\r{:^3.0f}%[{}->{}]{:.2f}s".format(c, a, b, t), end="")  # 总的进度条，和第二种类型
-    time.sleep(0.2)
+    time.sleep(0.1)
 print("\n" + "执行结束".center(len(scale) // 2, "-"))
